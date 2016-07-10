38bb93b4d0c91d59a7a1909e17ca95ca3880a612
rolandinsh <rolands@mediabox.lv>
2016-07-11 02:08:26 +0300

    Fixing WP 4.5 get_currentuserinfo compatibility issues

--- w3-total-cache.php
+++ w3-total-cache.php
@@ -2,10 +2,9 @@
 /*
 Plugin Name: W3 Total Cache
 Description: The highest rated and most complete WordPress performance plugin. Dramatically improve the speed and user experience of your site. Add browser, page, object and database caching as well as minify and content delivery network (CDN) to WordPress.
-Version: 0.9.4.1
+Version: 0.9.4.1.201607111
 Plugin URI: http://www.w3-edge.com/wordpress-plugins/w3-total-cache/
-Author: Frederick Townes
-Author URI: http://www.linkedin.com/in/w3edge
+Author: Frederick Townes, Rolands Umbrovskis
 Network: True
 */
 
